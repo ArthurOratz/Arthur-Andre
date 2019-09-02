724ae76206792b74d3618ddfa053a38bad9c80be
André Luiz Thiel <andre_thiel007@hotmail.com>
2019-09-02 19:38:20 -0300

    Classes

---
+++ Desafio_Agenda/Compromisso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Agenda
+{
+    public class Compromisso
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public String Titulo { get; set; }
+
+        [MaxLength(500)]
+        public String Local { get; set; }
+
+        [Required]
+        public DateTime dataInicio { get; set; }
+
+        public DateTime dataFim { get; set; }
+
+        public IList<Contato> contatos { get; set; }
+    }
+}
---
+++ Desafio_Agenda/Contato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Agenda
+{
+    public class Contato
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public String Nome { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public String Sobrenome { get; set; }
+
+        public DateTime Data { get; set; }
+
+        [MaxLength(20)]
+        public String Telefone { get; set; }
+
+        public IList<Compromisso> Compromissos { get; set; }
+
+    }
+}
