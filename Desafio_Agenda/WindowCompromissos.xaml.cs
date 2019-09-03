bf179ae51d34e06775fd0b2eb1d24f17775fced7
Arthur Oratz <arthur_oratz@hotmail.com>
2019-09-02 21:46:26 -0300

    Botão adicionar pessoas compromisso

--- Desafio_Agenda/WindowCompromissos.xaml.cs
+++ Desafio_Agenda/WindowCompromissos.xaml.cs
@@ -45,5 +45,10 @@ namespace Desafio_Agenda
         {
             this.Close();
         }
+
+        private void BtnAdicionarPessoa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
